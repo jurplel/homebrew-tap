1bea57e68a650992e656622868984be9f990b58e
Benjamin O <jeep70cp@gmail.com>
2026-04-18 16:40:14 -0700

    2.0

--- Casks/instant-space-switcher.rb
+++ Casks/instant-space-switcher.rb
@@ -1,6 +1,6 @@
 cask "instant-space-switcher" do
-  version "1.0"
-  sha256 "1d6ce054df0d5eea734d20afca81556818280e3f68d54e4f011ae4d69ae8aa9c"
+  version "2.0"
+  sha256 "e3c0c7d87bbf5e12cfafc8cfd80ae62c92c56c9988ba992b96a9453ac3672fb8"
 
   url "https://github.com/jurplel/InstantSpaceSwitcher/releases/download/v#{version}/InstantSpaceSwitcher-#{version}.dmg"
   name "InstantSpaceSwitcher"
