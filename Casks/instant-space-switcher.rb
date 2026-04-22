40ab319ef32b1279831799658081c0662341a82a
LangLangBart <92653266+LangLangBart@users.noreply.github.com>
2026-04-22 12:23:46 +0200

    remove file attribute

--- Casks/instant-space-switcher.rb
+++ Casks/instant-space-switcher.rb
@@ -11,5 +11,24 @@ cask "instant-space-switcher" do
 
   app "InstantSpaceSwitcher.app"
 
+  postflight do
+    ohai "Releasing #{token} from quarantine"
+    system_command("/usr/bin/xattr",
+                   args: [
+                     "-dr",
+                     "com.apple.quarantine",
+                     "#{appdir}/InstantSpaceSwitcher.app",
+                   ])
+  end
+
   zap trash: "~/Library/Preferences/com.interversehq.InstantSpaceSwitcher.plist"
+
+  caveats <<~EOS
+    Warning: macOS's Gatekeeper has been disabled for this Cask
+
+    According to the vendor, the Gatekeeper quarantine attribute breaks the app and needs to be removed. This Cask, `#{token}`, automatically removes the quarantine attribute. No further action is required.
+
+    For more information:
+    - https://docs.brew.sh/FAQ#why-cant-i-open-a-mac-app-from-an-unidentified-developer
+  EOS
 end
